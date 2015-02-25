55013f24cf7fa6ecd38a85ae4b5fb5663b1c4b5f
Hirotaka Ikoma <hikoma@gmail.com>
2015-02-25 17:29:14 +0900

    impl

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var _ = require('lodash-node');
+var bitcoin = require('bitcoinjs-lib');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {script: '76A91489ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA88AC', chunks: []});
+});
+
+/* POST home page. */
+router.post('/', function (req, res, next) {
+    var hexScript = req.body.script.replace(/\s/g, '');
+    var script = bitcoin.Script.fromHex(hexScript);
+    var chunks = script.chunks.map(function (chunk) {
+        if (Buffer.isBuffer(chunk)) {
+            return {word: '<data>', opcode: chunk.toString('hex')};
+        } else {
+            var word = _.findKey(bitcoin.opcodes, function (value) {
+                return value == chunk;
+            });
+            return {word: word, opcode: chunk.toString(16)};
+        }
+    });
+
+    res.render('index', {script: hexScript, chunks: chunks});
 });
 
 module.exports = router;
